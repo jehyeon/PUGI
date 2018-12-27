6ea8a5d680d980679753fd608bb86d61a4d9ae0d
hyeon62 <hyeon.rot@gmail.com>
2018-12-27 21:24:34 +0900

    Better crop text

--- image_crop/crop.py
+++ image_crop/crop.py
@@ -10,14 +10,14 @@ def draw_(image, wRatio, hRatio):
 
     draw = ImageDraw.Draw(origin)
     for vl in w_line:
-        draw.line((vl,0, vl,h), fill='red')
+        draw.line((vl,0, vl,height), fill='red')
     
     for hl in h_line:
-        draw.line((0,hl, w, hl), fill='blue')
+        draw.line((0,hl, width, hl), fill='blue')
 
     origin.show()
 
-# draw_(image, 17, 16)
+draw_(image, 20, 16)
 
 # 위 함수를 통해 weapon 좌표를 구함
 # weapon1 = (12/17, 2/16), weapon2 = (12/17, 5/16)
@@ -29,12 +29,19 @@ original = Image.open(image)
 w, h = original.size
 
 # Weapon Info 1
-left = w/17 * 12
+left = w/20 * 14
 top = h/16 * 2
-right = w/17 * 13
+right = w/20 * 15
 bottom = h/16 * 3
-crroped = original.crop((left, top, right, bottom))
+crroped1 = original.crop((left, top, right, bottom))
+crroped1.save('./text1.jpg')
 
 # Weapon Info 2
+# left = w/20 * 14
+# top = h/16 * 5
+# right = w/20 * 15
+# bottom = h/16 * 6
+# crroped2 = original.crop((left, top, right, bottom))
 
-crroped.save('./test.jpg')
+# crroped2.show()
+# crroped2.save('./test2.jpg')
--- pyOCR/ocr.py
+++ pyOCR/ocr.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from pytesseract import *
+import cv2
 
 def OCR(_image, lang='eng'):
     image = Image.open(_image)
@@ -10,4 +11,26 @@ def OCR(_image, lang='eng'):
 # print(OCR('text_image.png'))
 # print(OCR('text_image_kr.png', 'kor'))
 
-print(OCR('test.jpg'))
+# print(OCR('test.jpg'))
+
+def thresholding(_image):
+    gray = cv2.imread(_image, cv2.IMREAD_GRAYSCALE)
+    
+    # Enlarge 2x
+    height, width = gray.shape
+    gray_enlarge = cv2.resize(gray, (width*2, height*2), interpolation=cv2.INTER_LINEAR)
+
+    # Denoising
+    denoised = cv2.fastNlMeansDenoising(gray, h=10, searchWindowSize=21, templateWindowSize=7)
+
+    # blur = cv2.GaussianBlur(gray, (3,3), 0)
+    ret, thr = cv2.threshold(denoised, 127, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+    print(image_to_string(thr, lang='eng'))
+
+    cv2.imshow("thr", thr)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+thresholding('test1.jpg')
